ea435bdd77f9bd5556a9c206894208f93d36bceb
Juarez <juarezfrancojr@gmail.com>
2024-02-01 18:20:21 -0300

    comment test

--- test/galactic_hotkeys_test.dart
+++ test/galactic_hotkeys_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:galactic_hotkeys/galactic_hotkeys.dart';
+// import 'package:galactic_hotkeys/galactic_hotkeys.dart';
 
 void main() {
   test('adds one to input values', () {
